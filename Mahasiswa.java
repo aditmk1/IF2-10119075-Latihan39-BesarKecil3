68397196b873f09a0317f2cc189273d3ca12ea14
aditmk1 <73748815+aditmk1@users.noreply.github.com>
2020-11-07 21:49:59 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan39;
+
+/**
+ *
+ * @author
+ * NAMA     : aditiya musthafa kamil
+ * KELAS    : IF-2
+ * NIM      : 10119075
+ * Deskripsi Program : Class untuk objek Mahasiswa
+ * 
+ * 
+ */
+public class Mahasiswa {
+    public Integer nim, nilai;
+}
---
+++ Program.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan39;
+
+/**
+ *
+ * @author
+ * NAMA     : aditiya musthafa kamil
+ * KELAS    : IF-2
+ * NIM      : 10119075
+ * Deskripsi Program : Program ini berisi program nilai terbesar dan terkecil
+ * (OBJECT ORIENTED)
+ * 
+ */
+public class Program {
+    
+    public static void main(String[] args) {
+        Kelas kelas = new Kelas();
+        kelas.mulai();
+    }
+}
